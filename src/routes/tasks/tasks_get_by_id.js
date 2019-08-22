328fdde74ed40734badba51b02f5fa4409106a5b
Jucesar <juliocbcb@hotmail.com>
2019-08-22 10:11:18 -0300

    Rotas Done Undone

--- src/routes/tasks/tasks_get_by_id.js
+++ src/routes/tasks/tasks_get_by_id.js
@@ -1,35 +1,26 @@
 import { Task } from "../../models";
 
 const response_builder = data => {
-  console.log(data)
-  let response = {};
-  if (data.length >= 1) {
-    response = {
-      status: data.length >= 1 ? "200" : "401",
-      data: {
-        oid: data[0].oid ? data[0].oid : 0,
-        title: data[0].title ? data[0].title : "",
-        description: data[0].description ? data[0].description : ""
+  let response = {
+    status: data.length >= 1 ? "200" : "401",
+    data: {
+      oid: data[0].oid ? data[0].oid : 0,
+      title: data[0].title ? data[0].title : "",
+      description: data[0].description ? data[0].description : ""
+    },
+    links: [
+      {
+        rel: `/linkrels/tasks/${data[0].oid}/${data[0].done ? "un" : ""}done`,
+        uri: `/tasks/${data[0].oid}/${data[0].done ? "un" : ""}done`
       },
-      links: [
-        {
-          rel: `/linkrels/tasks/${data[0].oid}/${data[0].done ? "un" : ""}done`,
-          uri: `/tasks/${data[0].oid}/${data[0].done ? "un" : ""}done`
-        },
-        {
-          rel: `/linkrels/tasks/${data[0].oid}/${
-            data[0].delete ? "un" : ""
-          }delete`,
-          uri: `/tasks/${data[0].oid}/${data[0].delete ? "un" : ""}delete`
-        }
-      ]
-    };
-  } else {
-    response = {
-      status: "401",
-      data: "Não tem gentileza com esse número"
-    };
-  }
+      {
+        rel: `/linkrels/tasks/${data[0].oid}/${
+          data[0].delete ? "un" : ""
+        }delete`,
+        uri: `/tasks/${data[0].oid}/${data[0].delete ? "un" : ""}delete`
+      }
+    ]
+  };
   return response;
 };
 export default {
