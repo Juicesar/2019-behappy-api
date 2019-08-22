2fe71e0d5614d44e8ecd55c17137f6b2fce0a56c
Jucesar <juliocbcb@hotmail.com>
2019-08-22 10:18:37 -0300

    Revisitando POst e Delete

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -3,6 +3,36 @@ import knex from "../config/knex";
 const table_name = "tasks";
 
 class Task {
+  static delete(oid) {
+    return knex(table_name)
+      .where("oid", oid)
+      .andWhere("delete", false)
+      .update("delete", true)
+      .then(tasks_deleted => {
+        if (tasks_deleted > 0) return Task.getById(oid);
+        else return [];
+      })
+      .catch(error => []);
+  }
+  static undelete(oid) {
+    return knex(table_name)
+      .where("oid", oid)
+      .andWhere("delete", true)
+      .update("delete", false)
+      .then(tasks_undeleted => {
+        if (tasks_undeleted > 0) return Task.getById(oid);
+        else return [];
+      })
+      .catch(error => []);
+  }
+
+  static create(data) {
+    return knex(table_name)
+      .insert(data)
+      .then(oid => oid)
+      .catch(err => -1);
+  }
+
   static getAll() {
     return knex
       .from(table_name)
@@ -15,7 +45,10 @@ class Task {
     return knex(table_name)
       .where("oid", id)
       .select()
-      .then(results => Task.deserialize(results))
+      .then(results => {
+        if (results.length == 0) return [];
+        else return Task.deserialize(results);
+      })
       .catch(err => err);
   }
 
@@ -42,6 +75,7 @@ class Task {
   }
 
   static deserialize(json) {
+    if (json.length == undefined || json.length == 0) return {};
     return json.map(data => {
       let task = new Task();
       task.oid = data.oid ? data.oid : 0;
